e4d9ce7b746f5aac0bcec47b8f98e3f1d11b1dcc
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-06-06 23:44:39 +0300

    CategoriesController created

---
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Services.CategoryServices;
+
+namespace MultiShop.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+       
+    }
+}
