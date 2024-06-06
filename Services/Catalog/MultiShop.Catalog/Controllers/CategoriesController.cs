02105f6fe3ea34d6140816f2c698f94e8555492f
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-06-06 23:54:53 +0300

    CategoriesController => crud transactions were written

--- Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
+++ Services/Catalog/MultiShop.Catalog/Controllers/CategoriesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MultiShop.Catalog.Dtos.CategoryDtos;
 using MultiShop.Catalog.Services.CategoryServices;
 
 namespace MultiShop.Catalog.Controllers
@@ -8,6 +9,47 @@ namespace MultiShop.Catalog.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
-       
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList()
+        {
+            var values = await _categoryService.GetAllCategoryAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var values = await _categoryService.GetByIdCategoryAsync(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori başarıyla eklendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+            await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori başarıyla silindi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori başarıyla güncellendi");
+        }
+
     }
 }
