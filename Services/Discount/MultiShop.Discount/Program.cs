e5013c53dfee9a1216a4a7703ad0cdae4c7ccfd6
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-08-01 14:35:31 +0300

    OrderMicroservice (Onion Architecture) => Core

--- Services/Discount/MultiShop.Discount/Program.cs
+++ Services/Discount/MultiShop.Discount/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<DapperContext>();
 builder.Services.AddTransient<IDiscountService, DiscountService>();
 
-
+ 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
