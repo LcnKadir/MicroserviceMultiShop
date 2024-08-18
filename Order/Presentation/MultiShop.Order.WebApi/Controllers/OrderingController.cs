70702b3d0d097853886fa3a68288ca7393a9d29a
LcnKadir <144541247+LcnKadir@users.noreply.github.com>
2024-08-18 19:07:41 +0300

    'OrderMicroservice' was protected and testing operations were carried out

--- Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingController.cs
+++ Order/Presentation/MultiShop.Order.WebApi/Controllers/OrderingController.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
@@ -7,6 +8,7 @@ using MultiShop.Order.Application.Features.Mediator.Queries.OrderingQueries;
 
 namespace MultiShop.Order.WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class OrderingController : ControllerBase
@@ -48,7 +50,7 @@ namespace MultiShop.Order.WebApi.Controllers
         }
 
         [HttpPut]
-        public  async Task<IActionResult> UpdateORdering(UpdateOrderingCommand command)
+        public async Task<IActionResult> UpdateORdering(UpdateOrderingCommand command)
         {
             await _mediator.Send(command);
             return Ok("Sipariş başarıyla güncellendi");
